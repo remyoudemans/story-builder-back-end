99856ad5898f334f5ddc1b26db890e6a6be71423
Remy Oudemans <remyoudemans@gmail.com>
2018-09-03 23:00:04 +0200

    reorganized and renamed files

--- server.js
+++ server.js
@@ -49,6 +49,25 @@ app.get('/example-story', (req, res, next) => {
 	})
 })
 
+app.get('/query-stuff', (req, res, next) => {
+	const response = req.query.keys
+		? ['bible', 'tree', 'oscar'].map(key => {
+				if (req.query.keys.includes(key)) {
+					switch (key) {
+						case 'bible':
+							return 'My father was a disc jockey'
+						case 'tree':
+							return "For God's sake! Get down!"
+						default:
+							return "I wouldn't ask that if I were you"
+					}
+				}
+				return 'Ew'
+		  })
+		: null
+	res.json(response)
+})
+
 app.get('/url', (req, res, next) => {
 	res.json(['Tony', 'Lisa', 'Michael', 'Ginger', 'Food'])
 })
